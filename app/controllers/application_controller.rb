6c7c1240d65cfe95025e2fd5b79d8aad2fee7f77
Yony <arleybri18@gmail.com>
2018-12-09 15:28:48 -0500

    Se implementa login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+    def sign_in(user)
+        cookies.permanent.signed[:user_id] = user.id #se implanta cookie
+        @current_user = user #se crea variable de instancia con el usuario logueado
+    end
+
+    def sign_out
+        cookies.delete(:user_id)
+        @current_user = nil
+    end
+
+    private
+    def signed_in?
+        !current_user.nil?
+    end
+
+    helper_method :signed_in?
+
+    def current_user
+        @current_user ||= User.find(cookies.signed[:user_id]) if cookies.signed[:user_id]
+    rescue ActiveRecord::RecordNotFound
+    end
+
+    helper_method :current_user
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   resources :users, only: [:new, :create]
   resources :questions
   
+  get 'login', to: 'sessions#new'
+  post 'login',to: 'sessions#create'
+
   root "questions#index"
 end
