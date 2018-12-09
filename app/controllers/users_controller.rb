e896c1b41849a5b5d161cc1f20bf942b478df8da
Yony <arleybri18@gmail.com>
2018-12-09 14:38:42 -0500

    implementa registro de usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params) 
+        
+        if @user.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email,:password,:name)
     end
 
 end
