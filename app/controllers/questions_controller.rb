ae677bbbf1e27381a289261452fd2e52be9de6de
Yony <arleybri18@gmail.com>
2018-12-09 11:17:23 -0500

    Se crea la vista new para questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,34 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
 
+  def show
+    @question = Question.find(params[:id])
+  end
 
-    def index
-        @questions = Question.all
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to questions_path, notice: 'Tu pregunta ha sido publicada con exito!'
+    else
+      render :new
     end
+  end
 
-    def show
-        @question = Question.find(params[:id])
-    end
+
+
+  
+  private
+
+  def question_params
+    params.require(:question).permit(:topic, :description)
+  end
 end
