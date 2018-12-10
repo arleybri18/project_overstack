e7061ddeb844c5ce58fa7d6b9fabece166dbdbfe
Yony <arleybri18@gmail.com>
2018-12-09 19:45:55 -0500

    Agrega validaciones a User

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,9 @@
 
 class User < ApplicationRecord
     has_secure_password validations: false
+
+    validates :email, uniqueness: true, format: /@/
+    validates :password, presence: true, on: :create
+    validates :password, length: { in: 6..20 }, allow_nil: true
+    validates :name, presence: true
 end
